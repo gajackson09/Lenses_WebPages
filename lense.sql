9a4d7623f6184752ff2df0979d99940d0792b5de
JoCapo <97632697+JoCapo@users.noreply.github.com>
2023-03-05 21:37:31 -0500

    added php base php files via upload jc
    database interactions, can add functions to nav files or imbedded in the html doc.

---
+++ lense.sql
@@ -0,0 +1,20 @@
+USE `lense`;
+CREATE TABLE `users`(
+    `userID` int(11) AUTO_INCREMENT,
+    `email` varchar(225),
+    `username` varchar(225),
+    `password` varchar(225),
+    `firstName` varchar(225),
+    `lastName` varchar(225),
+    PRIMARY KEY(`userID`)
+);
+CREATE TABLE `photos`(
+    `photoID` int(11) AUTO_INCREMENT,
+    `userID` int(11),
+    `date` date(),
+    `caption` varchar(225),
+    PRIMARY KEY (`photoID`)
+)
+
+INSERT INTO `users` (`userID`, `email`, `username`, `password`, `firstName`, `lastName`) 
+    VALUES (1, 'example@email.com', 'usnm', 'pswd');
