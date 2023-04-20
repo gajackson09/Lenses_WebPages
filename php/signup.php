73d0334e48a5b2d1f6c97a5ae53c33773d77a913
JoCapo <97632697+JoCapo@users.noreply.github.com>
2023-04-20 17:52:36 +0000

    Apr 20, 2023, 1:52 PM

--- php/signup.php
+++ php/signup.php
@@ -5,12 +5,14 @@
     //    echo("user already exists?");
     //}else{echo()}
 
+
     if(validate()){
         addUser();
         // echo()
         header("Location: ../profilePage.php");
         
     }else{
+        header("Location: ../profilePage.php");
         echo("you done fuckt up");
     }
 
